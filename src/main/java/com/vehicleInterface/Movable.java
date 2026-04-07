f8dcf8556ebf5bea25611468e044aaa97046f8f9
Carles Conesa Mañosa <a251158cc@correu.escoladeltreball.org>
2026-04-07 18:29:45 +0200

    Implement Movable public interface

---
+++ src/main/java/com/vehicleInterface/Movable.java
@@ -0,0 +1,16 @@
+package com.vehicleInterface;
+
+public interface Movable {
+
+    public static final int CAR = 10;
+    public static final int MOTO = 8;
+    public static final int TRUCK = 7;
+    public static final int BIKE = 3;
+    public static final int CAR_CONSUM = 10;
+    public static final int MOTO_CONSUM = 4;
+    public static final int BIKE_CONSUM = 6;
+
+    public boolean move(char pos);
+
+    public boolean collision();
+}
